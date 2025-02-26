15a1584a11413f7ef618437b216329645eadacde
wanisshaari21 <wa852145@ucf.edu>
2025-02-26 15:33:50 -0500

    Enemies folder and Wanis Scene Update

---
+++ Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public Transform[] patrolPoints;
+    public float speed = 2f;
+    private int currentPointIndex = 0;
+    private Animator animator; 
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Patrol();
+    }
+
+    void Patrol()
+    {
+        if (patrolPoints.Length == 0) return;
+
+        transform.position = Vector2.MoveTowards(transform.position, patrolPoints[currentPointIndex].position, speed * Time.deltaTime);
+
+        animator.SetBool("isWalking", true);
+
+        if (transform.position.x < patrolPoints[currentPointIndex].position.x)
+            transform.localScale = new Vector3(-1, 1, 1);
+        else
+            transform.localScale = new Vector3(1, 1, 1);
+
+        if (Vector2.Distance(transform.position, patrolPoints[currentPointIndex].position) < 0.1f)
+        {
+            currentPointIndex = (currentPointIndex + 1) % patrolPoints.Length;
+        }
+    }
+}
+
+

b10e6cc46945e41436297a0bf7d105db6083d375
Jean Carlos <jeancarloslinlai@gmail.com>
2025-03-08 22:26:00 -0500

    ADDED TEXTBOXES

--- Assets/Scripts/TransitionToLevel1.cs
+++ Assets/Scripts/TransitionToLevel1.cs
@@ -3,13 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class SceneTransition : MonoBehaviour
 {
-    public string LevelOne; // Set this in the Inspector
+    public string Level1 = "Level-1"; // Set this in the Inspector
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("scientist"))
         { // Make sure your player has the "Player" tag
-            SceneManager.LoadScene(LevelOne);
+            SceneManager.LoadScene(Level1);
         }
     }
 }
